d9906320f59d48976fc6fda13fe81359551045f2
biswajit91237 <39646826+biswajit9123@users.noreply.github.com>
2018-12-07 14:47:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import discord
+from discord.ext import commands
+from discord.ext.commands.cooldowns import BucketType
 import asyncio
-import os 
-import random
 import colorsys
-from discord.ext.commands import Bot
+import random
+import platform
+from discord import Game, Embed, Color, Status, ChannelType
+import os
+import functools
 
 Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0x00ff00)
 client = commands.Bot(description="poko Official Bot", command_prefix=commands.when_mentioned_or("*"), pm_help = True)
