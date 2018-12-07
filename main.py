a0e6d62806225cb7ac4db08465a1d849c093b725
biswajit91237 <39646826+biswajit9123@users.noreply.github.com>
2018-12-07 13:40:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,16 @@ async def on_ready():
 async def on_member_join(member):
     role = discord.utils.get(member.server.roles, name='★彡-Guest-彡★')
     await client.add_roles(member, role)
-    
+
+@client.event
+async def on_member_join(member):
+    print("In our server" + member.name + " just joined")
+    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+    embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
+    embed.set_author(name='Welcome message')
+    embed.add_field(name = '__Welcome to Our Server__',value ='**Hope you will be active here. Check Our server rules and never try to break any rules. ',inline = False)
+    embed.set_image(url = 'https://media.giphy.com/media/OkJat1YNdoD3W/giphy.gif')
+    await client.send_message(member,embed=embed)
     
     
 client.run(os.getenv('Token'))
