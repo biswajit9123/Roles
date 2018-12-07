e3f592ba6a17e34b68f23c4a2bd8e304bf51e717
biswajit91237 <39646826+biswajit9123@users.noreply.github.com>
2018-12-07 12:59:58 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import discord
+import asyncio
+import os
+from discord.ext.commands import Bot
+from discord.ext import commands
+
+client = Bot(description="poko", command_prefix="*", pm_help = False)
+
+@client.event
+async def on_ready():
+	print('Logged in as '+client.user.name+'')
+	print('--------')
+	print('--------')
+	print('Started <Poko>') 
+	return await client.change_presence(game=discord.Game(name='<Roles>')) 
+
+
+
+@client.event
+async def on_member_join(member):
+    role = discord.utils.get(member.server.roles, name='rolename here')
+    await client.add_roles(member, role)
+    
+    
+    
+client.run(os.getenv('Token'))
